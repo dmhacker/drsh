fedcdeb15e65978e7f7444d76334ecef552d20e1
David Hacker <dmhacker@protonmail.com>
2020-12-21 19:01:00 -0800

    Throw out dummy code in command and replace with server startup

--- cmd/drshd/main.go
+++ cmd/drshd/main.go
@@ -1,71 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"os"
-	"os/exec"
-	"os/signal"
-	"syscall"
+    "log"
 
-	"github.com/creack/pty"
-	"golang.org/x/term"
+	"github.com/dmhacker/drsh/internal/server"
 )
 
-func test() error {
-	// Create arbitrary command.
-	c := exec.Command("bash")
-	fmt.Println(c)
-
-	// Start the command with a pty.
-	ptmx, err := pty.Start(c)
-	if err != nil {
-		return err
-	}
-	// Make sure to close the pty at the end.
-	defer func() { _ = ptmx.Close() }() // Best effort.
-
-	// Handle pty size.
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGWINCH)
-	go func() {
-		for range ch {
-			if err := pty.InheritSize(os.Stdin, ptmx); err != nil {
-				log.Printf("error resizing pty: %s", err)
-			}
-		}
-	}()
-	ch <- syscall.SIGWINCH // Initial resize.
-
-	// Set stdin in raw mode.
-	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }() // Best effort.
-
-	// Copy stdin to the pty and the pty to stdout.
-	go func() { _, _ = io.Copy(ptmx, os.Stdin) }()
-    buf := make([]byte, 1000)
-    for {
-        n, err := ptmx.Read(buf)
-        if err != nil {
-            fmt.Println(err)
-            break
-        }
-        if n == 0 {
-            break
-        }
-        fmt.Println(buf[:n])
-    }
-	// _, _ = io.Copy(os.Stdout, ptmx)
-
-	return nil
-}
-
 func main() {
-	if err := test(); err != nil {
-		log.Fatal(err)
-	}
+    serv, err := server.NewServer("dmhsurface", "redis://localhost:6379")
+    if err != nil {
+        log.Fatalln(err)
+    }
+    if err = serv.Start(); err != nil {
+        log.Fatalln(err)
+    }
+    log.Println("bye")
 }
