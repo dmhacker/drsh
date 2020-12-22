810147d16b754ff4356378cf8dec0d3044e4631d
David Hacker <dmhacker@protonmail.com>
2020-12-21 16:45:42 -0800

    Add handler methods for ping, handshake, pty resize, etc

--- internal/server/server.go
+++ internal/server/server.go
@@ -11,16 +11,15 @@ import (
 )
 
 type SimpleMessage struct {
-
 }
 
 type Server struct {
 	Name     string
 	Id       uuid.UUID
-	Sessions map[uuid.UUID]Session
+	Sessions map[uuid.UUID]*Session
 	Rdb      *redis.Client
 	Incoming chan *packet.Packet
-    Outgoing chan *packet.Packet
+	Outgoing chan *packet.Packet
 }
 
 var ctx = context.Background()
@@ -37,67 +36,110 @@ func NewServer(name string, url string) (*Server, error) {
 	return &Server{
 		Name:     name,
 		Id:       id,
-		Sessions: make(map[uuid.UUID]Session),
+		Sessions: make(map[uuid.UUID]*Session),
 		Rdb:      redis.NewClient(opt),
 	}, nil
 }
 
-func (serv *Server) StartReceiver() {
+func (serv *Server) HandlePing(sender uuid.UUID, seqNum uint32) {
+	resp := packet.Packet{}
+	resp.Sender = serv.Id[:]
+	resp.Recipient = sender[:]
+	resp.AckSeqNum = seqNum
+	resp.Success = true
+	resp.Name = serv.Name
+	serv.Outgoing <- &resp
+}
+
+func (serv *Server) HandleHandshake(sender uuid.UUID, seqNum uint32, rows uint32, cols uint32, xpixels uint32, ypixels uint32) {
+	resp := packet.Packet{}
+	resp.Sender = serv.Id[:]
+	resp.Recipient = sender[:]
+	resp.AckSeqNum = seqNum
+	if _, ok := serv.Sessions[sender]; ok {
+		resp.Success = false
+		resp.Error = "You are already connected to this server"
+	} else {
+		session, err := NewSession(rows, cols, xpixels, ypixels)
+		if err != nil {
+			resp.Success = false
+			resp.Error = "An error occurred: " + err.Error()
+		} else {
+			resp.Success = true
+			serv.Sessions[sender] = session
+		}
+	}
+	serv.Outgoing <- &resp
+}
+
+func (serv *Server) HandleHeartbeat(sender uuid.UUID, seqNum uint32, pckt *packet.Packet) {
+	// TODO: Implement heartbeat
+}
+
+func (serv *Server) HandleInput(sender uuid.UUID, payload []byte) {
+	serv.Sessions[sender].Send(payload)
+}
+
+func (serv *Server) HandlePty(sender uuid.UUID, rows uint32, cols uint32, xpixels uint32, ypixels uint32) {
+	serv.Sessions[sender].Resize(rows, cols, xpixels, ypixels)
+}
+
+func (serv *Server) StartHandler() {
 	for pckt := range serv.Incoming {
 		recipient, err := uuid.FromBytes(pckt.GetRecipient())
 		if err != nil {
 			log.Printf("Could not interpret incoming recipient ID: %s", err)
-            continue
+			continue
 		}
-        if recipient != serv.Id {
-            // Silently ignore any packets not intended for us
-            continue
-        }
+		if recipient != serv.Id {
+			// Silently ignore any packets not intended for us
+			continue
+		}
 		sender, err := uuid.FromBytes(pckt.GetSender())
 		if err != nil {
 			log.Printf("Could not interpret incoming sender ID: %s", err)
-            continue
+			continue
 		}
 		switch pt := pckt.GetType(); pt {
 		case packet.Packet_CLIENT_PING:
-            // TODO: Handle ping
+			serv.HandlePing(sender, pckt.GetSeqNum())
 		case packet.Packet_CLIENT_HANDSHAKE:
-            // TODO: Handle handshake
+			serv.HandleHandshake(sender, pckt.GetSeqNum(), pckt.GetPtyRows(), pckt.GetPtyCols(), pckt.GetPtyXpixels(), pckt.GetPtyYpixels())
 		case packet.Packet_CLIENT_HEARTBEAT:
-            // TODO: Handle heartbeat
+			// TODO: Handle heartbeat
 		case packet.Packet_CLIENT_INPUT:
-            // TODO: Handle input
+			serv.HandleInput(sender, pckt.GetPayload())
 		case packet.Packet_CLIENT_PTY:
-            // TODO: Handle tty resize
+			serv.HandlePty(sender, pckt.GetPtyRows(), pckt.GetPtyCols(), pckt.GetPtyXpixels(), pckt.GetPtyYpixels())
 		default:
 			log.Printf("Received invalid packet from %s.\n", sender.String())
 		}
 	}
 }
 
-func (serv* Server) StartSender() {
-    for pckt := range serv.Outgoing {
-        recipient, err := uuid.FromBytes(pckt.GetRecipient())
-        if err != nil {
+func (serv *Server) StartSender() {
+	for pckt := range serv.Outgoing {
+		recipient, err := uuid.FromBytes(pckt.GetRecipient())
+		if err != nil {
 			log.Printf("Could not interpret outgoing recipient ID: %s", err)
-            continue
+			continue
 		}
-        channel := "drsh:" + recipient.String()
-        serial, err := proto.Marshal(pckt)
-        if err != nil {
+		channel := "drsh:" + recipient.String()
+		serial, err := proto.Marshal(pckt)
+		if err != nil {
 			log.Printf("Could not marshal packet: %s", err)
-            continue
-        }
-        err = serv.Rdb.Publish(ctx, channel, serial).Err()
-        if err != nil {
+			continue
+		}
+		err = serv.Rdb.Publish(ctx, channel, serial).Err()
+		if err != nil {
 			log.Printf("Unable to publish packet: %s", err)
-            continue
-        }
-    }
+			continue
+		}
+	}
 }
 
 func (serv *Server) Start() error {
-	go serv.StartReceiver()
+	go serv.StartHandler()
 	go serv.StartSender()
 	// Main thread is responsible for listening to messages and passing them off to handlers
 	pubsub := serv.Rdb.Subscribe(ctx, "drsh:"+serv.Id.String())
@@ -108,7 +150,7 @@ func (serv *Server) Start() error {
 		}
 		pckt := packet.Packet{}
 		proto.Unmarshal([]byte(msg.Payload), &pckt)
-		serv.Incoming <- &pckt 
+		serv.Incoming <- &pckt
 	}
 }
 
--- internal/server/session.go
+++ internal/server/session.go
@@ -8,36 +8,42 @@ import (
 )
 
 type Session struct {
-	Pty         *os.File
+	Pty *os.File
 }
 
-func NewSession(sz *pty.Winsize) (*Session, error) {
+func NewSession(rows uint32, cols uint32, xpixels uint32, ypixels uint32) (*Session, error) {
+	sz := pty.Winsize{
+		Rows: uint16(rows),
+		Cols: uint16(cols),
+		X:    uint16(xpixels),
+		Y:    uint16(ypixels),
+	}
 	cmd := exec.Command("bash")
-	ptmx, err := pty.StartWithSize(cmd, sz)
+	ptmx, err := pty.StartWithSize(cmd, &sz)
 	if err != nil {
 		return nil, err
 	}
 	return &Session{
-		Pty:         ptmx,
+		Pty: ptmx,
 	}, nil
 }
 
 func (session *Session) Send(payload []byte) {
-    session.Pty.Write(payload)
+	session.Pty.Write(payload)
 }
 
 func (session *Session) Resize(rows uint32, cols uint32, xpixels uint32, ypixels uint32) {
-    sz := pty.Winsize{
-        Rows: uint16(rows),
-        Cols: uint16(cols),
-        X:    uint16(xpixels),
-        Y:    uint16(ypixels),
-    }
-    pty.Setsize(session.Pty, &sz)
+	sz := pty.Winsize{
+		Rows: uint16(rows),
+		Cols: uint16(cols),
+		X:    uint16(xpixels),
+		Y:    uint16(ypixels),
+	}
+	pty.Setsize(session.Pty, &sz)
 }
 
 func (session *Session) Receive(buffer []byte) (int, error) {
-    return session.Pty.Read(buffer)
+	return session.Pty.Read(buffer)
 }
 
 func (session *Session) Close() {
