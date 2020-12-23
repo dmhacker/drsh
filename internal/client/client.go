f54b2e44c447e7c604a25588aabbb29f05cd63a2
David Hacker <dmhacker@protonmail.com>
2020-12-23 09:37:06 -0800

    Add ability for client to select target server

--- internal/client/client.go
+++ internal/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"sync"
 	"time"
+    "strconv"
     "strings"
 
 	"github.com/dmhacker/drsh/internal/packet"
@@ -15,7 +16,8 @@ import (
 
 const (
 	PingStage = iota
-    InputStage
+    SelectStage 
+    HandshakeStage
 	ConnectStage
 )
 
@@ -105,13 +107,42 @@ func (clnt *Client) PingAll() {
         clnt.Proxy.SendPacket(&ping)
 	}
     if clnt.PingLeft > 0 {
-        clnt.Cnd.Wait()
         go (func() {
             time.Sleep(10 * time.Second)
             clnt.Mtx.Lock()
             clnt.Cnd.Signal()
             clnt.Mtx.Unlock()
         })()
+        clnt.Cnd.Wait()
+    }
+}
+
+func (clnt *Client) SelectServer() *uuid.UUID {
+	clnt.Mtx.Lock()
+	defer clnt.Mtx.Unlock()
+    if len(clnt.PingNames) == 0 {
+        fmt.Println("No available servers.")
+        return nil
+    }
+    clnt.Stage = SelectStage
+    selections := make([]uuid.UUID, len(clnt.PingNames))
+    fmt.Println("Available servers:")
+    i := 0
+    for servId, servName := range clnt.PingNames {
+        fmt.Printf("%d) %s (%s)\n", i + 1, servName, servId.String())
+        selections[i] = servId
+        i++
+    }
+    for {
+        fmt.Print("#? ")
+        var selection string
+        fmt.Scanln(&selection) 
+        j, err := strconv.ParseInt(selection, 0, 64)
+        if err != nil || j < 1 || j > int64(len(selections)) {
+            fmt.Println("Invalid selection. Try again.")
+            continue
+        }
+        return &selections[j - 1]
     }
 }
 
@@ -125,11 +156,19 @@ func (clnt *Client) StartTimeoutHandler() {
 }
 
 func (clnt *Client) Start() {
+    // Connect to Redis and run background routines
 	go clnt.StartTimeoutHandler()
 	clnt.Proxy.Start()
+    time.Sleep(250 * time.Millisecond) // TODO: Replace delay with channel signals
+    // Send out pings to determine available servers
     clnt.PingAll()
+    // Have the client select one of these servers
+    servId := clnt.SelectServer()
+    if servId == nil {
+        return
+    }
+    fmt.Println(servId)
 	// TODO: Implement
-    fmt.Println(clnt.PingNames)
 }
 
 func (clnt *Client) Close() {
